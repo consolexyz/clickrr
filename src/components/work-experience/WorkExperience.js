cef51e6eb5cc981c6d9fb75f5bb9da80c0ab9813
Moses <mosesobike@gmail.com>
2022-03-31 15:42:21 +0100

    added to on scroll animation to te work experience

--- src/components/work-experience/WorkExperience.js
+++ src/components/work-experience/WorkExperience.js
@@ -1,11 +1,42 @@
-import React  from 'react';
+import React, { useEffect}  from 'react';
+import {gsap} from 'gsap';
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import Data from './Data';
 import "./work-experience.css"
 
 const WorkExperience= ()=>{
 
-  const workexperience = Data.map(item => <div className='content-box'>
-    <div className='content' key={item.id}>
+  gsap.registerPlugin(ScrollTrigger);
+
+
+  useEffect(() =>{
+
+  
+    
+    const contents = gsap.utils.toArray(".content");
+
+        gsap.set(contents, {autoAlpha: 0, y: 50 });
+
+        contents.forEach((content, i) => {
+
+         const anim = gsap.to(content,{duration : 1, autoAlpha: 1, y : 0, paused:true});
+    
+        ScrollTrigger.create({
+          trigger: content,
+          end:"bottom bottom",
+          once:true,
+           onEnter: self =>{
+            self.progress === 1 ? anim.progress(1) :
+           anim.play()
+           }
+        })
+      });
+  },[])
+
+
+
+  const workexperience = Data.map(item => <div className='content-box' key={item.id}  >
+    <div className='content' >
     <h3>{item.title}</h3>
     <h4>{item.date}</h4> 
     <p className='content-description'>{item.description}</p>
